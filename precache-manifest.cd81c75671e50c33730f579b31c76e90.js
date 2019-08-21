8b1d7882adb6aaee9d2f1063904b4171c7c0e302
rusoyc <russell_ong1995@hotmail.com>
2019-08-21 11:58:38 +0800

    Updates

---
+++ precache-manifest.cd81c75671e50c33730f579b31c76e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2de56af05bb6b687863f20c7eba6033",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ea7e4b140b4ec225464f",
+    "url": "/calculator/static/css/main.07f0e505.chunk.css"
+  },
+  {
+    "revision": "b8e572187ffe54d26c60",
+    "url": "/calculator/static/js/2.55c02506.chunk.js"
+  },
+  {
+    "revision": "ea7e4b140b4ec225464f",
+    "url": "/calculator/static/js/main.e626d436.chunk.js"
+  },
+  {
+    "revision": "31b5d9eb319227676886",
+    "url": "/calculator/static/js/runtime~main.f2393964.js"
+  }
+]);
